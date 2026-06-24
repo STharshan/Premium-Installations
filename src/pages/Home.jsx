add06a9e4b6cdd564bcd40a9a3e7100e63d19aa9
shandiru <msddiru@gmail.com>
2026-06-24 22:26:06 +0530

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import HeroSection from '../Component/Home/HeroSection'
 import Services from '../Component/Home/Service'
 import WhyChooseUs from '../Component/Home/Whychoose'
 import WhoWeAre from '../Component/Home/WhoWeAre'
-import Contact from '../Component/Home/ContactSection'
+
 import About from '../Component/Home/about'
 import Testimonials from '../Component/Home/TestimonialsCarousel'
 const Home = () => {
@@ -22,7 +22,7 @@ const Home = () => {
       <WhoWeAre />
       <WhyChooseUs />
       <FAQSection />
-      <Contact />
+  
       <ContactSection />
     </main>
   )
